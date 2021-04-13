4f800cb3dfd08e346204b96c0268a7a8d40967ae
Anderson Dauer <>
2021-04-13 21:23:57 +0000

    Version 0.2.0

--- teacherLogin.js
+++ teacherLogin.js
@@ -1,13 +1,29 @@
 //Version 0.1.1
 //Last Updated: 4/7/2021
 
-function clickHandle(evt, emailAddress)
+function logIn(evt, emailAddress, passWord)
 {
-  //let gulleyEmail, shanksEmail, enteredEmail;
   var gulleyEmail = "agulley@g.risd.org";
   var shanksEmail = "ashanks@g.risd.org";
 
   var enteredEmail = document.getElementById("username").value;
-  
-  alert(enteredEmail);
+
+  var gulleyPswrd = "gulley-pip";
+  var shanksPswrd = "shanks-pip";
+
+  var enteredPswrd = document.getElementById("password").value;
+
+  if(enteredEmail == gulleyEmail || enteredPswrd == gulleyPswrd)
+  {
+    //alert("Email found");
+
+    location.href = 'https://risddigitaltimesheet.pip2021.repl.co/teacherGulley.html';
+  }
+
+  if(enteredEmail == shanksEmail || enteredEmail == shanksPswrd)
+  {
+    //alert("Email found");
+
+    location.href = 'https://risddigitaltimesheet.pip2021.repl.co/teacherShanks.html';
+  }
 }
