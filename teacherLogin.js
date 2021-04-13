91289d3e7e695743eeac1486b342402317c0d304
Anderson Dauer <>
2021-04-13 20:18:56 +0000

    Version 0.1.2

---
+++ teacherLogin.js
@@ -0,0 +1,13 @@
+//Version 0.1.1
+//Last Updated: 4/7/2021
+
+function clickHandle(evt, emailAddress)
+{
+  //let gulleyEmail, shanksEmail, enteredEmail;
+  var gulleyEmail = "agulley@g.risd.org";
+  var shanksEmail = "ashanks@g.risd.org";
+
+  var enteredEmail = document.getElementById("username").value;
+  
+  alert(enteredEmail);
+}
